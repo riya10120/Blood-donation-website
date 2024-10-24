c84d5d53203bb33da2016d8dc872be63a6804d76
riya10120 <reemasherin10120@gmail.com>
2024-10-24 21:47:12 +0530

    changed database name

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 	session_start();
 
 	// connect to database
-	$db = mysqli_connect('localhost', 'root', '', 'multi_login');
+	$db = mysqli_connect('localhost', 'root', '', 'db_movie');
 
 	// variable declaration
 	$username = "";
